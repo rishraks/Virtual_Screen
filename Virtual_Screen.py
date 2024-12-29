d4233b19c0564a97792bea54ee6a76e04b7de2f5
Rishav Rakesh <rishavrakesh8@gmail.com>
2024-12-29 19:58:43 +0530

    Added Color Pallet

--- Virtual_Screen.py
+++ Virtual_Screen.py
@@ -1,14 +1,14 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-
+import math
 
 # Web Cam Initialization
 cap = cv2.VideoCapture(0)
 
 # Hand Tracking
 mp_hand = mp.solutions.hands
-hands = mp_hand.Hands(max_num_hands = 1, static_image_mode = False, min_detection_confidence = 0.7, min_tracking_confidence = 0.7)
+hands = mp_hand.Hands(max_num_hands = 1, static_image_mode = False, min_detection_confidence = 0.5, min_tracking_confidence = 0.8)
 
 # Index Finger Position
 index_new_pos = (0,0)
@@ -52,8 +52,16 @@ def is_thumb_extended(landmarks, handedness):
     else:
         return landmarks[4].x < landmarks[3].x
 
-
 selection_mode = False
+
+# Color Coordinates
+color_bbox=[{"color_centre":(200,25),"radius":22,"color":(0,0,255)},
+            {"color_centre":(280,25),"radius":22,"color":(0,255,0)},
+            {"color_centre":(360,25),"radius":22,"color":(255,0,0)},
+            {"color_centre":(440,25),"radius":22,"color":(0,255,255)},
+            {"color_centre":(520,25),"radius":22,"color":(255,165,0)},
+            {"color_centre":(600,25),"radius":22,"color":(255,0,255)},  
+            {"color_centre":(680,25),"radius":22,"color":(255,165,0)}]
 
 while True:
     ret, frame = cap.read()
@@ -76,23 +84,32 @@ while True:
 
         cv2.rectangle(frame,b_start,b_end,(128, 128, 128),2)
         
+        for box in color_bbox:
+            cv2.circle(frame, box["color_centre"], box["radius"], box["color"],-1)
         
         if results.multi_hand_landmarks:
             for handlms,handedness in zip(results.multi_hand_landmarks,results.multi_handedness):
+                
+                index_tip = handlms.landmark[8]
+                index_pos = int(index_tip.x*w),int(index_tip.y*h)
+                    
+                middle_tip = handlms.landmark[12]
+                middle_pos = int(middle_tip.x*w),int(middle_tip.y*h)
                 
                 # Detects hand type
                 handtype = handedness.classification[0].label
                 
                 # Condition to check index,middle finger extension Selection Mode
                 if is_extended(handlms.landmark,8,7) and is_extended(handlms.landmark,12,11) and is_bent(handlms.landmark,16,15) and is_bent(handlms.landmark,20,19) and is_thumb_bent(handlms.landmark,handtype):
-                    cv2.putText(frame,"Selection mode",(10,30),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+                    cv2.putText(frame,"Select Color",(10,35),cv2.FONT_HERSHEY_COMPLEX,0.7,(0,0,255),2)
+                    cv2.rectangle(frame,index_pos,middle_pos,(255,255,0),-1)
                     index_new_pos = (0,0)
                     smooth_pos = (0,0)
                     selection_mode = True
                 
                 # Condition to check Palm for Erase
                 elif is_extended(handlms.landmark,8,7) and is_extended(handlms.landmark,12,11) and is_extended(handlms.landmark,16,15) and is_extended(handlms.landmark,20,19) and is_thumb_extended(handlms.landmark,handtype):
-                    cv2.putText(frame, "Erasing", (10, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255), 2)
+                    cv2.putText(frame, "Erasing", (10, 35), cv2.FONT_HERSHEY_COMPLEX, 0.7, (0, 0, 255), 2)
                     palm_center = handlms.landmark[9]
                     palm_pos = int(palm_center.x * w), int(palm_center.y * h)
                     cv2.circle(frame, palm_pos, 50, (0, 0, 255), 2)
@@ -100,18 +117,27 @@ while True:
 
                 # Condition to check index finger extension for paint
                 elif is_extended(handlms.landmark,8,7) and is_bent(handlms.landmark,12,11) and is_bent(handlms.landmark,16,15) and is_bent(handlms.landmark,20,19) and is_thumb_bent(handlms.landmark,handtype):
-                    index_tip = handlms.landmark[8]
-                    index_pos = int(index_tip.x*w),int(index_tip.y*h)
-                    
                     if selection_mode:
                         smooth_pos = index_pos
                         index_new_pos = index_pos
                         selection_mode = False
                     
                     smooth_pos = (int(alpha*smooth_pos[0]+(1-alpha)*index_pos[0]),int(alpha*smooth_pos[1]+(1-alpha)*index_pos[1]))
+                    cv2.circle(frame, smooth_pos, 8, brush_color, -1)
                     
-                    cv2.circle(frame, smooth_pos, 8, (255,0,255), -1)
+                    # Condition to check if index and middle fingers are inside the circles
+                    for box in color_bbox:
+                        region = box["color_centre"]
+                        radius = box["radius"]
+                        
+                        finger_pos = smooth_pos  # Position of the index finger tip
+                        distance = math.sqrt((finger_pos[0] - region[0]) ** 2 + (finger_pos[1] - region[1]) ** 2)
+                            
+                        if distance<= radius:
+                            brush_color = box["color"]
+                            cv2.circle(frame, smooth_pos, 8, brush_color, -1)
                     
+                    # Condition to check if index finger is inside the painting region boundary
                     if b_start[0]<=smooth_pos[0]<=b_end[0] and b_start[1]<=smooth_pos[1]<=b_end[1]:
                         if index_new_pos != (0, 0) and abs(smooth_pos[0] - index_new_pos[0]) < 30 and abs(smooth_pos[1] - index_new_pos[1]) < 30:
                             cv2.line(canvas,index_new_pos,smooth_pos,brush_color,3)
